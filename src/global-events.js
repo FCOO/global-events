409f5abba56c289ead6fc69c65c8b746690825d4
NielsHolt <niels@steenbuchholt.dk>
2016-11-30 20:43:51 +0100

    Bug fix

--- src/global-events.js
+++ src/global-events.js
@@ -1,115 +1,113 @@
 /****************************************************************************
-	global-events, a plugin to administrate any events
+    global-events, a plugin to administrate any events
 
-	(c) 2015, FCOO
+    (c) 2015, FCOO
 
-	https://github.com/FCOO/global-events
-	https://github.com/FCOO
+    https://github.com/FCOO/global-events
+    https://github.com/FCOO
 
 ****************************************************************************/
 
-(function ($, window, document, undefined) {
-	"use strict";
+(function ($, window/*, document, undefined*/) {
+    "use strict";
 
-	function GlobalEvents( ) {
-		var i;
-		this.events = {};
+    function GlobalEvents( ) {
+        this.events = {};
 
-		this._loop = function _loop( eventName, func, reverse ){
-			this.events[eventName] = this.events[eventName] || []; 		
-			var lgd = this.events[eventName].length;
-			if (reverse){
-				for (i=lgd-1; i>=0; i-- )
-					if (func( this.events[eventName][i], i, this.events[eventName] ))
-					  break;
-			} else {
-				for (i=0; i<lgd; i++ )
-					if (func( this.events[eventName][i], i, this.events[eventName] ))
-					  break;
-			}
-		};
+        this._loop = function( eventName, func, reverse ){
+            this.events[eventName] = this.events[eventName] || [];         
+            var i, lgd = this.events[eventName].length;
+            if (reverse){
+                for (i=lgd-1; i>=0; i-- )
+                    if (func( this.events[eventName][i], i, this.events[eventName] ))
+                        break;
+            } 
+            else {
+                for (i=0; i<lgd; i++ )
+                    if (func( this.events[eventName][i], i, this.events[eventName] ))
+                        break;
+            }
+        };
 
-		this.on = function on(eventNames, callback, context, options){
-			var eventName, i;
-			eventNames = ( eventNames || "" ).match( (/\S+/g) ) || [ "" ];
-			for (i=0; i<eventNames.length; i++ ){
-				eventName = eventNames[i];
-				if (eventName){
-					this.events[eventName] = this.events[eventName] || []; 		
-					this.events[eventName].push( {
-						callback: callback,
-						context	: context || null,
-						options	: $.extend( {once:false, first:false, last:false}, options ) 
-					});
-				}
-			}
-		};
+        this.on = function(eventNames, callback, context, options){
+            var i, eventName = ( eventNames || "" ).match( (/\S+/g) ) || [ "" ];
+            for (i=0; i<eventNames.length; i++ ){
+                eventName = eventNames[i];
+                if (eventName){
+                    this.events[eventName] = this.events[eventName] || [];         
+                    this.events[eventName].push( {
+                        callback: callback,
+                        context : context || null,
+                        options : $.extend( {once:false, first:false, last:false}, options ) 
+                    });
+                }
+            }
+        };
 
-		this.once				= function once(			eventName, callback, context ) { this.on( eventName, callback, context, {	once:true 						} ); };
-		this.onFirst		= function onFirst(		eventName, callback, context ) { this.on( eventName, callback, context, {						 first:true	} ); };
-		this.onLast			= function onLast(		eventName, callback, context ) { this.on( eventName, callback, context, {						 last:true	} ); };
-		this.onceFirst	= function onceFirst(	eventName, callback, context ) { this.on( eventName, callback, context, { once:true, first:true	} ); };
-		this.onceLast		= function onceFirst(	eventName, callback, context ) { this.on( eventName, callback, context, { once:true, last:true	} ); };
+        this.once      = function( eventName, callback, context ) { this.on( eventName, callback, context, { once:true             } ); };
+        this.onFirst   = function( eventName, callback, context ) { this.on( eventName, callback, context, {            first:true } ); };
+        this.onLast    = function( eventName, callback, context ) { this.on( eventName, callback, context, {            last:true  } ); };
+        this.onceFirst = function( eventName, callback, context ) { this.on( eventName, callback, context, { once:true, first:true } ); };
+        this.onceLast  = function( eventName, callback, context ) { this.on( eventName, callback, context, { once:true, last:true  } ); };
 
-		this.off = function off(eventNames, callback, context){
-			var eventName, i, _loop_func;
-			eventNames = ( eventNames || "" ).match( (/\S+/g) ) || [ "" ];
-			_loop_func = function( eventObj, index, list ){
-				if ( (callback == eventObj.callback) &&
-					(!context || (context == eventObj.context)) ){ 
-					list.splice(index, 1);
-					return true;
-				}
-			};
+        this.off = function(eventNames, callback, context){
+            var eventName, i, _loop_func;
+            eventNames = ( eventNames || "" ).match( (/\S+/g) ) || [ "" ];
+            _loop_func = function( eventObj, index, list ){
+                if ( (callback == eventObj.callback) &&
+                    (!context || (context == eventObj.context)) ){ 
+                    list.splice(index, 1);
+                    return true;
+                }
+            };
 
-			for (i=0; i<eventNames.length; i++ ){
-				eventName = eventNames[i];
-				if (eventName){
-					this._loop( eventName, _loop_func );
-				}
-			}
-		};
+            for (i=0; i<eventNames.length; i++ ){
+                eventName = eventNames[i];
+                if (eventName){
+                    this._loop( eventName, _loop_func );
+                }
+            }
+        };
 
 
-		this.fire = function fire( eventName /*, arg1, arg2, .., argN */ ){ 
-			var newArguments = [];
-			for (var i=1; i < arguments.length; i++) {
-				newArguments.push(arguments[i]);
-			}
+        this.fire = function( eventName /*, arg1, arg2, .., argN */ ){ 
+            var newArguments = [];
+            for (var i=1; i < arguments.length; i++) {
+                newArguments.push(arguments[i]);
+            }
 
-			//Fire the functions marked 'first'
-			this._loop( eventName, function( eventObj ){ 
-				if (eventObj.options.first)
-					eventObj.callback.apply( eventObj.context, newArguments );	  
-			});
+            //Fire the functions marked 'first'
+            this._loop( eventName, function( eventObj ){ 
+                if (eventObj.options.first)
+                    eventObj.callback.apply( eventObj.context, newArguments );      
+            });
 
-			//Fire the functions not marked 'first' or 'last'
-			this._loop( eventName, function( eventObj ){ 
-				if (!eventObj.options.first && !eventObj.options.last)
-					eventObj.callback.apply( eventObj.context, newArguments );	  
-			});
+            //Fire the functions not marked 'first' or 'last'
+            this._loop( eventName, function( eventObj ){ 
+                if (!eventObj.options.first && !eventObj.options.last)
+                    eventObj.callback.apply( eventObj.context, newArguments );      
+            });
 
-			//Fire the functions marked 'last'
-			this._loop( eventName, function( eventObj ){ 
-				if (eventObj.options.last)
-					eventObj.callback.apply( eventObj.context, newArguments );	  
-			});
-			
-			//Remove all functions marked 'once'
-			this._loop( eventName, function( eventObj, index, list ){ 
-				if (eventObj.options.once)
-					list.splice(index, 1);
-			}, true);
-		};
+            //Fire the functions marked 'last'
+            this._loop( eventName, function( eventObj ){ 
+                if (eventObj.options.last)
+                    eventObj.callback.apply( eventObj.context, newArguments );      
+            });
+            
+            //Remove all functions marked 'once'
+            this._loop( eventName, function( eventObj, index, list ){ 
+                if (eventObj.options.once)
+                    list.splice(index, 1);
+            }, true);
+        };
 
-		this.trigger	= function(){ this.fire( arguments );				};
-		this.one			=	function(){	this.once( arguments );				};
-		this.oneFirst	= function(){	this.onceFirst( arguments );	};
-		this.oneLast	=	function(){	this.onceLast( arguments  );	};
-
-	}
+        this.trigger  = function(){ this.fire( arguments );      };
+        this.one      = function(){ this.once( arguments );      };
+        this.oneFirst = function(){ this.onceFirst( arguments ); };
+        this.oneLast  = function(){ this.onceLast( arguments  ); };
+    }
   
-  // expose access to the constructor
-  window.GlobalEvents = GlobalEvents;
+    // expose access to the constructor
+    window.GlobalEvents = GlobalEvents;
 
 }(jQuery, this, document));
